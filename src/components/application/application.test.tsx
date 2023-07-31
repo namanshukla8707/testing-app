3357bf21cc1b11992b0934c3ad8df8cba80854ac
namanshukla8707 <113860823+namanshukla8707@users.noreply.github.com>
2023-08-01 00:07:35 +0530

    testing completed

--- src/components/application/application.test.tsx
+++ src/components/application/application.test.tsx
@@ -52,6 +52,6 @@ describe("Application", () => {
 
     const submitButtonElement = screen.getByRole("button");
     expect(submitButtonElement).toBeInTheDocument();
-    expect(submitButtonElement).not.toBevksvEnabled();
+    expect(submitButtonElement).toBeDisabled();
   });
 });
