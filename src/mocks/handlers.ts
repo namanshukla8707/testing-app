fac929fd96d08f0ebb37e98905083a8e2839f5e5
namanshukla8707 <113860823+namanshukla8707@users.noreply.github.com>
2023-07-31 23:22:10 +0530

    learn mock testing api functions hooks component functions error

---
+++ src/mocks/handlers.ts
@@ -0,0 +1,22 @@
+import { rest } from "msw";
+export const handlers = [
+  rest.get(
+    "https://jsonplaceholder.typicode.com/users",
+    (request, response, ctx) => {
+      return response(
+        ctx.status(200),
+        ctx.json([
+          {
+            name: "Bruce Wayne",
+          },
+          {
+            name: "Naman Shukla",
+          },
+          {
+            name: "Princess Diana",
+          },
+        ])
+      );
+    }
+  ),
+];
