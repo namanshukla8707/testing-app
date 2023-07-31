8c51398948f037b1780e4fce58894fd344b888ed
namanshukla8707 <113860823+namanshukla8707@users.noreply.github.com>
2023-07-31 23:56:34 +0530

    naman

--- src/components/application/application.test.tsx
+++ src/components/application/application.test.tsx
@@ -52,6 +52,6 @@ describe("Application", () => {
 
     const submitButtonElement = screen.getByRole("button");
     expect(submitButtonElement).toBeInTheDocument();
-    expect(submitButtonElement).not.toBeEnabled();
+    expect(submitButtonElement).not.toBemnsdfEnabled();
   });
 });
